1addd3c6908ab84bbbdd5ce2b9828b149fbde6d5
Jefferson Silva <jeffersonsantsil@gmail.com>
2022-10-31 23:09:48 -0300

    Penúltima aula do módulo I

---
+++ praticando.js
@@ -0,0 +1,21 @@
+let idade = 19;
+const idadeDois = 26;
+let nome = "Jefferson";
+
+// console.log(idade + " " + nome);
+console.log(idadeDois);
+
+
+if(idade >= 18){
+    idade = 29;
+    console.log("Maior de idade");
+    // idadeDois = 27;
+}else{
+    console.log("Menor de idade");
+}
+
+console.log(idade);
+console.log(idadeDois);
+
+const d = '';
+console.log(d == 0)
---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome = "Jefferson Silva";
+const idade = 2022-1992;
+const cidadeNascimento = "Vitória de Santo Antão"
+const estadoNacimento = "Pernambuco";
+
+const apresentacao = `Oi meu nome é ${nome}, tenho ${idade} anos, nasci na cidade de ${cidadeNascimento} no estado de ${estadoNacimento}.`;
+
+console.log(apresentacao);
